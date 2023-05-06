736cff13b62ad6838bcb27b3bfff50dd5ff2182e
yashdkadam <kadamdyash55@gmail.com>
2023-05-06 21:04:19 +0530

    intial commit

--- routes/movies.js
+++ routes/movies.js
@@ -18,7 +18,7 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   // const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  // if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findById(req.body.genreId);
   if (!genre) return res.status(400).send("Invalid genre.");
